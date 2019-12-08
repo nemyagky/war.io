818651741d00be9074a86d57d791ef302a0cfaa7
Magmiv <ivan.mag.web@gmail.com>
2019-12-08 13:28:29 +0000

    Webpack refactoring + TypeScript integration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,28 @@
 const path = require('path')
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-let conf = {
-   entry: './app/js/gameloop.js',
-   output: {
-      'path': path.resolve(__dirname, './dist'),
-      'filename': 'main.js',
-      publicPath: 'dist/'
-   },
-   devServer: {
-      overlay: true
-   }, 
-   module: {
-      rules: [
-         {
-            test: /\.js$/,
-            loader: 'babel-loader'
-            /* exclude: '/node_modules' */
-         },
-         {
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-               fallback: "style-loader",
-               use: "css-loader"
-             })
-         },
-         {
-            test: /\.sass$/,
-            use: ExtractTextPlugin.extract({
-               fallback: "style-loader",
+module.exports = (paths) => {
+   return {
+      entry: './app/js/gameloop.ts',
+      output: {
+         'path': path.resolve(__dirname, './dist'),
+         'filename': 'main.js',
+         publicPath: 'dist/'
+      },
+      devServer: {
+         overlay: true
+      },
+      module: {
+         rules: [
+            {
+               test: /\.scss$/,
+               include: paths,
                use: [
-                  "css-loader",
-                  "sass-loader"
+                  'style-loader',
+                  'css-loader',
+                  'sass-loader'
                ]
-             })
-        }
-      ]
-   },
-   plugins: [
-      new ExtractTextPlugin("styles.css"),
-   ]
-}
-
-module.exports = (env, options) => {
-   let production = options.mode === 'production';
-
-   conf.devtool = production ? false : 'eval-sourcemap';
-
-   return conf;
+            }
+         ]
+      },
+   }
 }
